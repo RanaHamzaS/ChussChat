74bc77f989177380ee8120558936eb59b545e45c
RanaHamzaS <hamzathegreat110@gmail.com>
2024-03-09 18:59:26 +0500

    First UI Changes

--- polling.js
+++ polling.js
@@ -2,16 +2,14 @@ let chatPanel = document.querySelector('#chat-panel');
 setInterval(() => {
     fetch(ipAddress + '/get-messages').then(lock => lock.json()).then((message) => {
         chatPanel.innerHTML = "";
-        console.log(message);
         for (let index = 0; index < message.length; index++) {
             const element = message[index];
             const messageContent = element.message;
             const sentBy = element.sentBy;
             const createdAt = element.createdAt;
             if (messageContent != undefined && sentBy != undefined && createdAt != undefined) {
-                chatPanel.innerHTML += `<li>${sentBy}: ${messageContent}</li> <div class="timestamp">${createdAt}</div></li>`   
+                chatPanel.innerHTML += `<li>${sentBy}: ${messageContent}</li> <div class="timestamp">${createdAt}</div></li>`
             }
         }
-        chatPanel.scrollTop = chatPanel.scrollHeight
     })
 }, 250);
